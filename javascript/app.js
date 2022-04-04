04b13657169a14377c226423e033f797dcbeaaac
reynaldo-bejarano <reynaldo.bjsz@gmail.com>
2022-04-04 09:34:09 -0600

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,174 @@
+const score = document.getElementById("score");
+
+const btnPaper = document.getElementById("btn-paper");
+const btnScissors = document.getElementById("btn-scissor");
+const btnRock = document.getElementById("btn-rock");
+
+const main = document.getElementById("main");
+const picked = document.getElementById("picked");
+const btnRules = document.getElementById("btn-rules");
+const cardRules = document.getElementById("card-rules");
+const cardRules__ = document.getElementById("card-rules__");
+
+const btnPlayer = document.getElementById("btn-player");
+const playerIcon = document.getElementById("player-icon");
+const btnHouse = document.getElementById("btn-house");
+const houseIcon = document.getElementById("house-icon");
+
+const winnerBlock = document.getElementById("winnerBlock");
+const winner = document.getElementById("winner");
+const btnReset = document.getElementById("btn-reset");
+
+let numberScore = 0;
+let propsNumber = 0;
+let randomNumero = 0;
+
+const array = [
+  {
+    figura: "paper",
+    icon: "icon-paper",
+    border: "border-primary",
+  },
+  {
+    figura: "scissors",
+    icon: "icon-scissors",
+    border: "border-warning",
+  },
+  {
+    figura: "rock",
+    icon: "icon-rock",
+    border: "border-danger",
+  },
+];
+
+document.addEventListener("click", (e) => {
+  closeCardRules(e);
+});
+
+btnPaper.addEventListener("click", (e) => {
+  Game(0);
+});
+
+btnScissors.addEventListener("click", (e) => {
+  Game(1);
+});
+
+btnRock.addEventListener("click", (e) => {
+  Game(2);
+});
+
+const Game = (props) => {
+  styleGame(props);
+  switch (props) {
+    case 0:
+      Paper();
+      break;
+    case 1:
+      Scissors();
+      break;
+    case 2:
+      Rock();
+      break;
+  }
+};
+
+btnReset.addEventListener("click", () => {
+  btnPlayer.classList.remove(array[propsNumber].border);
+  playerIcon.classList.remove(array[propsNumber].icon);
+
+  btnHouse.classList.remove(array[randomNumero].border);
+  houseIcon.classList.remove(array[randomNumero].icon);
+
+  main.classList.remove("d-none");
+  picked.classList.add("d-none");
+});
+
+const randomNumber = () => {
+  return Math.floor(Math.random() * 3);
+};
+
+const Paper = () => {
+  if (randomNumero == 1) {
+    numberScore--;
+    score.innerHTML = `${numberScore}`;
+    winner.innerHTML = "YOU LOSE";
+  } else if (randomNumero === 0) {
+    winner.innerHTML = "DRAW";
+  } else {
+    numberScore++;
+    score.innerHTML = `${numberScore}`;
+    winner.innerHTML = "YOU WIN";
+  }
+};
+
+const Scissors = () => {
+  if (randomNumero == 2) {
+    numberScore--;
+    score.innerHTML = `${numberScore}`;
+    winner.innerHTML = "YOU LOSE";
+  } else if (randomNumero === 1) {
+    winner.innerHTML = "DRAW";
+  } else {
+    numberScore++;
+    score.innerHTML = `${numberScore}`;
+    winner.innerHTML = "YOU WIN";
+  }
+};
+
+const Rock = () => {
+  if (randomNumero == 0) {
+    numberScore--;
+    score.innerHTML = `${numberScore}`;
+  } else if (randomNumero === 2) {
+    winner.innerHTML = "DRAW";
+  } else {
+    numberScore++;
+    score.innerHTML = `${numberScore}`;
+    winner.innerHTML = "YOU WIN";
+  }
+};
+
+const styleGame = (props) => {
+  main.classList.add("d-none");
+  picked.classList.remove("d-none");
+
+  btnPlayer.classList.add(array[props].border);
+  playerIcon.classList.add(array[props].icon);
+
+  propsNumber = props;
+  randomNumero = randomNumber();
+
+  btnHouse.classList.add(array[randomNumero].border);
+  houseIcon.classList.add(array[randomNumero].icon);
+};
+
+const closeCardRules = (e) => {
+  if (
+    e.target.id === "btn-rules" ||
+    e.target.id === "btn-close" ||
+    e.target.id === "btn-close__"
+  ) {
+    cardRules.classList.toggle("d-none");
+    cardRules__.classList.toggle("d-none");
+  }
+};
+
+window.addEventListener("resize", (e) => {
+  if (window.innerWidth < "576") {
+    winnerBlock.classList.remove("top-50");
+    winnerBlock.classList.add("top-100");
+  } else {
+    winnerBlock.classList.remove("top-100");
+    winnerBlock.classList.add("top-50");
+  }
+});
+
+window.addEventListener("load", (e) => {
+  if (window.innerWidth < "576") {
+    winnerBlock.classList.remove("top-50");
+    winnerBlock.classList.add("top-100");
+  } else {
+    winnerBlock.classList.remove("top-100");
+    winnerBlock.classList.add("top-50");
+  }
+});
